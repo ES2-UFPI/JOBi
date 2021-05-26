eb42ee2438f49e26a40522b25cc7cb1a339434fe
PedroLucca <pedrolucca27@gmail.com>
2021-05-26 14:47:39 -0300

    testando erros de conexao #24

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ class App{
             res.setHeader("Access-Control-Allow-Credentials", "true");
             res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
             res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-            app.use(cors());
+            this.app.use(cors());
             next();
         });
     }
