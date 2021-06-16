f76260da53a13903b565d2f695c54c533e327ff8
PedroLucca <pedrolucca27@gmail.com>
2021-06-16 10:10:57 -0300

    testando erro actions

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import {server} from './http';
 import './src/websocket/socket';
 
 
-//rodar server
+//rodar server - teste actions
 server.listen(3333, () => {
     console.log("Rodando na 3333");
 });
