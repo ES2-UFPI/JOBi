90c7364c20bc189b4d25928fb9af9e9e8167923b
Naum Celestino <naum_celestino@hotmail.com>
2021-06-27 15:47:40 -0300

    alteração na rota de home #48

--- backend/src/routes/vagaRoutes.js
+++ backend/src/routes/vagaRoutes.js
@@ -6,7 +6,7 @@ const router = new Router()
 router.post("/", vagaController.store);
 router.get("/contratante/:id", vagaController.index);
 router.get("/:id", vagaController.select_id);
-router.get("/:id/:categoria", vagaController.select_home);
+router.get("/home/:categoria", vagaController.select_home);
 router.put("/:id", vagaController.update);
 
 export default router;
