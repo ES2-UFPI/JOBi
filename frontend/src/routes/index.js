a454fd93e2e9903676358532999dd8248556b10c
mizaelgoonfs <miza14pg@hotmail.com>
2021-06-28 11:05:28 -0300

    Conexão de chat (backend e frontend) #51

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -4,7 +4,7 @@ import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Cadastro from '../pages/Cadastro';
 import Prestador from '../pages/Prestador';
-import Contratante from '../pages/Contratante';
+import Contratante_temp from '../pages/Contratante_temp';
 import User from '../pages/User';
 import Chat_p from '../pages/Chat_p';
 import Chat_c from '../pages/Chat_c';
@@ -16,7 +16,7 @@ function Routes(){
             <MyRoute exact path="/login" component={Login} />
             <MyRoute exact path="/cadastro" component={Cadastro} />
             <MyRoute exact path="/prestador" component={Prestador}/>
-            <MyRoute exact path="/contratante" component={Contratante}/>
+            <MyRoute exact path="/contratante" component={Contratante_temp}/>
             <MyRoute exact path="/prestador/chat" component={Chat_p} />
             <MyRoute exact path="/contratante/chat" component={Chat_c} />
             <MyRoute exact path="/user" component={User} isClosed />
